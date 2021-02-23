0e958907b74a537401b9c35feaaba3e94cb9212c
Clara Mendes <claraplanejamento@gmail.com>
2021-02-23 10:09:47 -0300

    :sparkles: adds uri 1002

---
+++ uri/1002.cpp
@@ -0,0 +1,28 @@
+// The formula to calculate the area of a circumference is defined as A = π . R2. Considering to this problem that π = 3.14159:
+// Calculate the area using the formula given in the problem description.
+
+// Input
+// The input contains a value of floating point (double precision), that is the variable R.
+
+// Output
+// Present the message "A=" followed by the value of the variable, as in the example bellow, with four places after the decimal point. Use all double precision variables. Like all the problems, don't forget to print the end of line after the result, otherwise you will receive "Presentation Error".
+
+#include <iostream>
+#include <iomanip>
+#include <math.h>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    double pi = 3.14159;
+    double R;
+    double A;
+    
+    cin >> R;
+    A = pi*pow(R,2);
+
+    cout << fixed << setprecision(4) << "A=" << A << endl;
+ 
+    return 0;
+}
