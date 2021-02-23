468e1cf01b394efc4d09fd3117ee798c5bf7a2c3
Clara Mendes <claraplanejamento@gmail.com>
2021-02-23 09:51:09 -0300

    :sparkles: adds uri 1001

---
+++ uri/1001.cpp
@@ -0,0 +1,26 @@
+// Read 2 variables, named A and B and make the sum of these two variables, assigning its result to the variable X. Print X as shown below. Print endline after the result otherwise you will get “Presentation Error”.
+
+// Input
+// The input file will contain 2 integer numbers.
+
+// Output
+// Print the letter X (uppercase) with a blank space before and after the equal signal followed by the value of X, according to the following example.
+
+// Obs.: don't forget the endline after all.
+
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    int A, B;
+    int sum;
+
+    cin >> A;
+    cin >> B;
+    sum = A + B;
+    cout << "X = " << sum << std::endl;
+ 
+    return 0;
+}
