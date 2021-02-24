9a1957804e54bbf46405d09d8cc016829289f538
Clara Mendes <claraplanejamento@gmail.com>
2021-02-24 10:13:18 -0300

    :sparkles: adds uri 1004

---
+++ uri/1004.cpp
@@ -0,0 +1,33 @@
+// Read two integer values. After this, calculate 
+//the product between them and store the result in 
+//a variable named PROD. Print the result like the 
+//example below. Do not forget to print the end of 
+//line after the result, otherwise you will receive 
+//“Presentation Error”.
+
+// Input
+// The input file contains 2 integer numbers.
+
+// Output
+// Print the message "PROD" and PROD according to 
+//the following example, with a blank space before
+// and after the equal signal.
+
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    int number1;
+    int number2;
+    int prod;
+
+    cin >> number1;
+    cin >> number2;
+    prod = number1*number2;
+
+    cout << "PROD = " << prod << endl;
+ 
+    return 0;
+}
