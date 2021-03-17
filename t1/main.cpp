4046d45d4065106ed1cb54b0eee209bed185442f
Clara Mendes <claraplanejamento@gmail.com>
2021-03-17 20:04:34 -0300

    :sparkles: adds first project

---
+++ t1/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include "Dominios.h"
+#include "Entidades.h"
+#include "TestesDominio.h"
+
+using namespace std;
+
+int main() {
+    /// Criação de objetos de teste
+
+}
