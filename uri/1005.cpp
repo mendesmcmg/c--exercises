22acf18d6e030fd1142ad91cf380e3555ef84cff
Clara Mendes <claraplanejamento@gmail.com>
2021-10-31 18:00:27 -0300

    adds uri 1005

---
+++ uri/1005.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std; 
+
+int main() {
+  double a, b, avg;
+  cin >> a >> b;
+  avg = ((a*3.5) + (b*7.5))/11.0;
+  cout << fixed << setprecision(5) << "MEDIA = " << avg << endl;
+}
